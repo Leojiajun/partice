736ec43cd6e80fae3554d7c5ad3048be3342342b
jiajun.wu <jiajun.wu@sao.so>
2018-06-08 11:34:29 +0800

    pratice0608

---
+++ src/practiceone/Pracetice1OnTestng.java
@@ -0,0 +1,53 @@
+package practiceone;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Pracetice1OnTestng {
+	@BeforeTest
+	public void beforeTest(){
+		System.out.println("beforetest");
+	}
+	
+	@BeforeClass//比如像登录操作
+	public void beforeClass(){
+		System.out.println("beforeclass");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod(){
+		System.out.println("beforemethod");
+	}
+	
+	@Test
+	public void test1(){
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println("test2");
+	}
+	
+	@AfterMethod
+	public void afterMethod(){
+		System.out.println("aftermethod");
+	}
+	
+	@AfterClass//登出操作
+	public void afterClass(){
+		System.out.println("afterclass");
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		System.out.println("aftertest");
+	}
+	
+	
+}
